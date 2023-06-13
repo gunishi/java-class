e81f89ac1d0722451a60f8026fd58cd2d2a057c8
Gunishi Nathoo <gunishinathoo@gmail.com>
2023-06-13 10:59:44 +0100

    Hello World in Netbeans

---
+++ Classwork/HelloWorld/src/main/java/com/jgn/helloworld/Hello.java
@@ -0,0 +1,18 @@
+
+
+/**
+ *
+ * @author gunishinathoo
+ * email:gunishinathoo@gmail.com
+ * date: 13/06/23
+ * purpose: my first Hello World program in an IDE
+ */
+
+package com.jgn.helloworld;
+
+public class Hello {
+    public static void main(String[] args) {
+       System.out.println("Hello, World!");
+    }
+    
+}
